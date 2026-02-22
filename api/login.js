356664c5f575e0b07f8e2c55adfdaafb4c1ed71b
jocelyn360 <sarafrahman017@gmail.com>
2026-02-22 22:59:53 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,7 +3,7 @@ export default function handler(req,res){
   const LICENSES=[
     {id:"demo10",pass:"demo10",exp:"2026-03-01"},
       {id:"demo1",pass:"demo1",exp:"2026-03-01"},
-    {id:"client1",pass:"client123",exp:"2026-12-31"}
+    {id:"client1",pass:"client123",exp:"2027-12-31"}
   ];
   const u=LICENSES.find(x=>x.id===id&&x.pass===pass);
   if(!u) return res.json({ok:false,msg:"Invalid"});
